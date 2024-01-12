98ae884fb70f85d848865c38468c8d5f1e398640
BAD-99 <brandon.andrew.davis@gmail.com>
2024-01-12 05:11:50 -0700

    Add basic classes

---
+++ src/projects.js
@@ -0,0 +1,8 @@
+// todos, name, color
+export default class Project{
+    constructor(todos, name, color){
+        this.todos = todos;
+        this.name = name;
+        this.color = color;
+    }
+}
